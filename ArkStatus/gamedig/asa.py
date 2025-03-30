c6f76f0bd45e3d44fc8e1a3fa4d0e8a264b70d41
MillerLoren <lorenmillerp@gmail.com>
2025-03-30 12:37:48 -0600

    Refactor ASAQuery to utilize OpenGSQL API for server queries and implement access token management

--- ArkStatus/gamedig/asa.py
+++ ArkStatus/gamedig/asa.py
@@ -1,25 +1,64 @@
 # gamedig/asa.py
+import time
 from opengsq import Source
 import asyncio
 
 class ASAQuery:
+    """
+    ASAQuery class to query ASA servers using the OpenGSQL API."
+    """
+
+
+    pre_query_required = True
+    name = "asa"
+
+    _client_id = "xyza7891muomRmynIIHaJB9COBKkwj6n"
+    _client_secret = "PP5UGxysEieNfSrEicaD1N2Bb3TdXuD7xHYcsdUHZ7s"
+    _deployment_id = "ad9a8feffb3b4b2ca315546f038c3ae2"
+    _grant_type = "client_credentials"
+    _external_auth_type = ""
+    _external_auth_token = ""
+    _access_token = ""
+
     @staticmethod
-    async def query(ip: str, port: int) -> dict:
-        try:
-            source = Source()
-            result = await source.info(ip, port)
+    async def pre_query(self):
+        ASAQuery._access_token = await opengsq.EOS.get_access_token(
+            client_id=self._client_id,
+            client_secret=self._client_secret,
+            deployment_id=self._deployment_id,
+            grant_type=self._grant_type,
+            external_auth_type=self._external_auth_type,
+            external_auth_token=self._external_auth_token,
+        )
+    @staticmethod
+    async def query(self):
+        if not ASA._access_token:
+            await self.pre_query()
 
-            server = {
-                "name": result.get("name", "Unknown Server"),
-                "map": result.get("map", "Unknown"),
-                "folder": result.get("folder", "Unknown"),
-                "game": result.get("game", "Unknown"),
-                "id": result.get("id", 0),
-                "players": result.get("players", 0),
-                "maxplayers": result.get("max_players", 0),
-                "bots": result.get("bots", 0),
-            }
-            return server
+        host, port = str(self.kv["host"]), int(str(self.kv["port"]))
+        eos = opengsq.EOS(
+            host, port, self._deployment_id, ASA._access_token, self.timeout
+        )
+        start = time.time()
+        info = await eos.get_info()
+        ping = int((time.time() - start) * 1000)
 
-        except Exception as e:
-            raise Exception(f"Query failed: {e}")
+        # Credits: @dkoz https://github.com/DiscordGSM/GameServerMonitor/pull/54/files
+        attributes = dict(info.get("attributes", {}))
+        settings = dict(info.get("settings", {}))
+
+        result = {
+            "name": attributes.get("CUSTOMSERVERNAME_s", ""),
+            "map": attributes.get("MAPNAME_s", ""),
+            "password": attributes.get("SERVERPASSWORD_b", False),
+            "numplayers": info.get("totalPlayers", 0),
+            "numbots": 0,
+            "maxplayers": settings.get("maxPublicPlayers", 0),
+            "players": None,
+            "bots": None,
+            "connect": f"{host}:{port}",
+            "ping": ping,
+            "raw": info,
+        }
+
+        return result
